14ba60e6e0b50afa757c7afe68c20e99bb69022e
prithvipoojari <poojari1prithvi@gmail.com>
2025-08-25 18:07:55 +0530

    added groups to manage objects

--- main.py
+++ main.py
@@ -9,14 +9,21 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock_object = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player_object = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player_object.draw(screen)
-        player_object.update(dt)
+        for draw_item in drawable:
+            draw_item.draw(screen)
+
+        
+        updatable.update(dt)
         pygame.display.flip()
         passed_time = clock_object.tick(60)
         dt = passed_time/1000
